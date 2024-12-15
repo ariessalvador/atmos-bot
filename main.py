c111bd239ca6343e28f00fbdafcbe4b41abb16fc
hiryac <john.salvador@neu.edu.ph>
2024-12-16 07:19:38 +0800

    add headless feature

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--remote-debugging-port=9222")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--headless=new")
+
 
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
